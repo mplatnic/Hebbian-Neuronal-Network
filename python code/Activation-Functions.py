edb2addaa8b7e83db40da4baa29e57783b24401f
Michel <12084024+platnic@users.noreply.github.com>
2019-11-09 10:13:17 +0200

    Create Activation-Functions.py

---
+++ python code/Activation-Functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1.0/(1.0 + np.exp(-x))
+
+def sigmoid_prime(x):
+    return sigmoid(x)*(1.0-sigmoid(x))
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_prime(x):
+    return 1.0 - x**2
