d317d066c05c2320754d0454f3dce26cb42c4e65
Michel <12084024+platnic@users.noreply.github.com>
2019-11-09 15:02:00 +0200

    Update ANN 2-1 OR-Sigmoid bias.py

--- python code/ANN 2-1 OR-Sigmoid bias.py
+++ python code/ANN 2-1 OR-Sigmoid bias.py
@@ -9,8 +9,7 @@
 
 
 import numpy as np
-
-bias = -2
+import random
 
 # sigmoid function
 # this nonlinearity maps a function called a "sigmoid"
@@ -18,42 +17,58 @@ bias = -2
 def nonlin(x, deriv=False):
     if (deriv == True):
         return x * (1 - x)
-    return 1 / (1 + np.exp(-x-bias))
+    return 1 / (1 + np.exp(-x))
 
 
-# input dataset
+# input dataset  [4,2]
 X = np.array([[0, 0],
               [0, 1],
               [1, 0],
-              [1, 1]])
+              [1, 1]])  
 
-# output dataset
+# output dataset  [4,1]
 y = np.array([[0, 1, 1, 1]]).T  # T is for mattrix transpose
 
 # seed random numbers to make calculation deterministic. (1) is the sequence used for random
 np.random.seed(1)
 
 # initialize weights randomly with mean 0
-syn0 = 2 * np.random.random((2, 1)) - 1    # 2 neurons input
+syn0 = 2 * np.random.random((2, 1)) - 1    # [2,1]    2 neurons input
+# initialize bias
+bias = random.random()
 
-for iter in range(1000):
+for iter in range(50000):
     # forward propagation
     l0 = X
-    l1 = nonlin(np.dot(l0, syn0))  # l1 = nonlin (l0 x syn0), matrix-matrix multiplication
+    # l1 = nonlin (l0 x syn0), matrix-matrix multiplication
+    # [4,1] = [4,2].[2,1] + [4,1]
+    l1 = nonlin(np.dot(l0, syn0)) + bias
 
-    # how much did we miss?
+    # how much did we miss?  [4,1]  (target-output)
     l1_error = y - l1
 
     # multiply how much we missed by the
     # slope of the sigmoid at the values in l1
+    # [4,1]  = [4,1]* const
     l1_delta = l1_error * nonlin(l1, True)
 
     # update weights
+    # mu is learning rate
+    # Syn0new = syn0 + mu*(Partial derivative L1_error and syn0)
+    # Syn0new = syn0 + mu*(l1_error*sigmoid_deriv(l1)*l0)
+    # Syn0new = syn0 + mu*(l1_delta*l0)
+    # [2,1] = [2,4].[4,1]
     syn0 += np.dot(l0.T, l1_delta)
+
+    # update bias
+    bias += np.sum(l1_delta)
 
 
 print("Output After Training:")
 print(l1)
+
 print("Weight:")
 print(syn0)
+print("Bias:")
+print(bias)
 
